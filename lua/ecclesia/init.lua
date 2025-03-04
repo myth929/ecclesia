bdd2fa7e40aa6df2c8d9ee83baf9ef54060a8139
gaozhengguo <gaozhengguo@MacBook-Pro.local>
2025-03-04 20:53:24 +0800

    expand and enhance demo coverage

--- lua/ecclesia/init.lua
+++ lua/ecclesia/init.lua
@@ -1,11 +1,13 @@
 local Utils = require("ecclesia.utils")
-local api = vim.api
 
 return {
     -- init plugin
     setup = function()
         if vim.fn.has("nvim-0.10") == 1 then
-            local async, message = vim.uv.new_async(function() Utils.debug("run success") end)
+            local async, message = vim.uv.new_async(function()
+                Utils.debug("run success")
+                vim.schedule(function() vim.notify("run success !!!", vim.log.levels.INFO, {}) end)
+            end)
             if async then
                 async:send()
             else
@@ -13,11 +15,6 @@ return {
             end
         else
             Utils.debug("The version of neovim needs to be at least 0.10!! you can use branch legacy")
-            api.nvim_notify(
-                "The version of neovim needs to be at least 0.10!! you can use branch legacy",
-                vim.log.levels.WARN,
-                {}
-            )
         end
     end,
 }
